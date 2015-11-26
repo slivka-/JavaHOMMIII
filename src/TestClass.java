8cb7e952db636db29632e68d56ac330fe041b6c4
Irozin <irozin7@gmail.com>
2015-11-26 16:04:30 +0100

    Change directory

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		MapGrid map = new MapGrid(5, 5, 50, 50);
-		DrawMapGrid draw = new DrawMapGrid(map);
+		final DrawMapGrid draw = new DrawMapGrid(map);
 		
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
